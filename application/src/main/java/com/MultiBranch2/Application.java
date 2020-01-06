a3fd1a590fed671e5c0014d47142af224ecd657f
mapatil1 <56800032+mapatil1@users.noreply.github.com>
2020-01-06 12:12:02 +0530

    first commit

---
+++ application/src/main/java/com/MultiBranch2/Application.java
@@ -0,0 +1,25 @@
+package com.MultiBranch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.sap.cloud.sdk", "com.MultiBranch2"})
+@ServletComponentScan({"com.sap.cloud.sdk", "com.MultiBranch2"})
+public class Application extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure( final SpringApplicationBuilder application )
+    {
+        return application.sources(Application.class);
+    }
+
+    public static void main( final String[] args )
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ application/src/main/java/com/MultiBranch2/models/HelloWorldResponse.java
@@ -0,0 +1,13 @@
+package com.MultiBranch2.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class HelloWorldResponse
+{
+    @JsonProperty("hello")
+    private final String name;
+
+    public HelloWorldResponse( final String name ) {
+        this.name = name;
+    }
+}
